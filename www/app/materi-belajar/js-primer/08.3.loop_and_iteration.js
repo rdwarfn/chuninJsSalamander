dac815a4d3ad2c3e7e4a6274bd7050dd42ce2fc9
rdwarfn <r.arifin123@gmail.com>
2024-09-30 10:43:55 +0700

    update: add do..while and while statement

--- www/app/materi-belajar/js-primer/08.3.loop_and_iteration.js
+++ www/app/materi-belajar/js-primer/08.3.loop_and_iteration.js
@@ -10,6 +10,13 @@
  */
 
 /**
+ * operator yg biasa di pakai atau berhubungan dgn materi:
+ * - Sama dengan / equals: == / ===
+ * - Kurang dari (less than): <
+ * - Lebih dari (more than): >
+ */
+
+/**
  * 1. For Statement
  * Perulangan For mengulang hingga kondisi tertentu
  * bernilai salah atau false.
@@ -103,3 +110,101 @@ for (let i = 0; i < dataA.length; i++) {
     }
 
 }
+
+/**
+ * 2. Do...While Statement
+ * Perulangan `do...while` mengulang hingga kondisi tertentu
+ * bernilai salah atau false.
+ * 
+ * do...while statement terlihat sbb:
+ * 
+ * ```
+ * do
+ *   statement
+ * while (condition);
+ * ``` 
+ * 
+ * 2.1 Statement`
+ * `statement` slalu dieksekusi satu kali sblm kondisi diperiksa.
+ * Untuk meng-eksekusi bberapa `statements`, kita bisa menggunakan
+ * block statement ({}) untuk menggelompokan `statements` trsb.
+ * 
+ * 2.2 Condition
+ * Jika kondisi bernilai benar, pernyataan akan dieksekusi lagi.
+ * Pada akhir setiap eksekusi, kondisi diperiksa. Ketika kondisi salah,
+ * eksekusi berhenti, dan kontol berpindah ke pernyataan stlh `do...while`
+ */
+
+/**
+ * Example do...while
+ * Pada contoh berikut, `do loop` mengulang setidaknya satu kali dan
+ * menggulang sampai i tidak lagi kurang dari 5
+ */
+var i = 0;
+
+// console.log
+do {
+    i = i++;
+    
+    // bentuk lain dr i = i + 1, adlh i = i++ atau i += 1;
+    // i += 1;
+
+    // console.log(i) 1,2,3,4
+    // con
+} while (i > 5);
+
+
+/**
+ * 3. While Statement
+ * Perulangan `while` akan trus dieksekusi `statement` nya selama
+ * kondisi yg ditentukan bernilai benar atau true.
+ * 
+ * While statement terlihat sbb:
+ * 
+ * ```
+ * while (condition)
+ *   statement
+ * ```
+ * 
+ * Jika kondisi menjadi salah atau false, statement dalam perulangan
+ * berhenti dieksekusi dan kontrol berpindah ke pernyataan stlah
+ * perulangan.
+ * 
+ * Kondisi (condition) akan dicek trlbh dahulu sblm pernyataan dlm
+ * perulangan dieksekusi. Jika kondisi bernilai benar ato true,
+ * pernyataan dieksekusi dan kondisi diuji ato dicek lagi.
+ * Jika kondisi bernilai salah, eksekusi berhenti dan kontol
+ * diteruskan ke pernyataan stlh while.
+ */
+
+
+/**
+ * Example 1 while
+ * Pada contoh berikut
+ */
+
+let n = 0
+let x = 0;
+
+while (n < 3) {
+    n++; // 1, 2, 3
+    x += n; // 1, 3, 6
+    // sama dengan kayak dibawah
+    // x = x + n
+}
+
+/**
+ * Pada setiap iterasi, perulangan menambah 'n' dan menambahkan
+ * nilai trsbt ke 'x'. Oleh karena itu, x dan n memiliki niali sbb:
+ * 
+ * - Setelah iterasi pertama pass:
+ *   n = 1 dan x = 1
+ * - Setelah iterasi kedua pass:
+ *   n = 2 dan x = 3
+ * - Setelah iterasi ketiga pass:
+ *   n = 3 dan x = 6
+ */
+
+/**
+ * Example 2 while
+ */
