614031ac1a0bdf2f8332d9ca9ad141c63ca0f57a
rdwarfn <r.arifin123@gmail.com>
2024-10-14 14:30:20 +0700

    add: materi type checking

---
+++ www/app/materi-belajar/js-primer/11.type_checking.js
@@ -0,0 +1,17 @@
+/**
+ * Checking the type of a variable
+ * can be done as follows:
+ */
+
+//String:
+typeof variable === "string"
+//Number:
+typeof variable === "number"
+//Boolean:
+typeof variable === "boolean"
+//Object:
+typeof variable === "object"
+//null:
+variable === null
+//null or undefined:
+variable == null
