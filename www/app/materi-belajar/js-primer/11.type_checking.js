1a213311458ca59d09f5e8eb14dfb8d4af4b4313
rdwarfn <r.arifin123@gmail.com>
2024-10-15 11:24:47 +0700

    update: Catatan obj constructor & obj array

--- www/app/materi-belajar/js-primer/11.type_checking.js
+++ www/app/materi-belajar/js-primer/11.type_checking.js
@@ -15,3 +15,63 @@ typeof variable === "object"
 variable === null
 //null or undefined:
 variable == null
+
+/**
+ * Obj constructor
+ * 
+ * yg pake constructor new,
+ * dia sifat nya obj kalo di typeof
+ */
+variable = String("string obj")
+
+const apiComents = {
+    id: String("1234sdfsafd"),
+
+    // bener tapi kurang tepat: obj
+    // variable: new String("str obj"),
+
+    // bener
+    // variable: String("str pass"),
+
+    // Salah
+    variable: Number(1000)
+}
+
+// if (apiComents.variable != null)
+if (!apiComents.variable) {
+
+    if (typeof apiComents.variable == "object") {
+
+        console.log("handle str obj")
+
+    } else if (typeof apiComents.variable == "string") {
+
+        console.log("handle str normal")
+
+    } else {
+        console.log("handle variable yg kemungkinan tdk sesuai tipe nya")
+    }
+}
+
+/**
+ * Array case
+ */
+const arryComments = []
+
+// 1.
+if (typeof arryComments === "object") {
+    console.log("handle kurang pasti")
+
+    if (Array.isArray(arryComments)) {
+        console.log("handle array yg valid")
+
+    } else {
+        console.log("tipe obj yg bkn array")
+    }
+
+}
+
+// 2.
+if (Array.isArray(arryComments)) {
+    console.log("handle array yg valid")
+}
