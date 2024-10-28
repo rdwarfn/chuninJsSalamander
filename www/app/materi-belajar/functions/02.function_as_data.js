5578b07462b3f793a64bce5ecdaeac914ee9f64d
rdwarfn <r.arifin123@gmail.com>
2024-10-28 11:40:20 +0700

    update: tambah build fungsi array materi function as data

--- www/app/materi-belajar/functions/02.function_as_data.js
+++ www/app/materi-belajar/functions/02.function_as_data.js
@@ -19,7 +19,7 @@ say("Kalo bulan (sutena) bisa ngomongs")
  * Kita dapat passing parameter pd saat di assign variable
  */
 
-const dataPosts = [{id: "1", post: "Halo ini post 1", createtAt: "2024-10-24T07:17:51.023Z"}]
+const dataPosts = [{ id: "1", post: "Halo ini post 1", createtAt: "2024-10-24T07:17:51.023Z" }]
 const dataArray = function () {
     return [{
         "id": "1",
@@ -57,4 +57,29 @@ var validateDataForAge = function(data) {
 
 var errorHandleForAge = function(error) {
     console.log("Error while processing age");
+}
+
+// Array fungsi bawaan JS (includes)
+// untuk tugas function as data
+const arryCategory = []
+const dataBook = [
+    { "title": "Mockingjay (The Hunger Games #3)", "category": "Default", "description": "My name is Katniss Everdeen.Why am I not dead?I should be dead.Katniss Everdeen, girl on fire, has survived, even though her home has been destroyed. Gale has escaped. Katniss's family is safe. Peeta has been captured by the Capitol. District 13 really does exist. There are rebels. There are new leaders. A revolution is unfolding.It is by design that Katniss was rescued fr My name is Katniss Everdeen.Why am I not dead?I should be dead.Katniss Everdeen, girl on fire, has survived, even though her home has been destroyed. Gale has escaped. Katniss's family is safe. Peeta has been captured by the Capitol. District 13 really does exist. There are rebels. There are new leaders. A revolution is unfolding.It is by design that Katniss was rescued from the arena in the cruel and haunting Quarter Quell, and it is by design that she has long been part of the revolution without knowing it. District 13 has come out of the shadows and is plotting to overthrow the Capitol. Everyone, it seems, has had a hand in the carefully laid plans--except Katniss.The success of the rebellion hinges on Katniss's willingness to be a pawn, to accept responsibility for countless lives, and to change the course of the future of Panem. To do this, she must put aside her feelings of anger and distrust. She must become the rebels' Mockingjay--no matter what the personal cost. ...more", "price": "\u00a320.44" }
+]
+
+// loop & iteration
+for (let i = 0; i < dataBook.length; i++) {
+    const getBook = dataBook[i];
+
+    // cek apakah categori sudah ada di arryCategory
+    // cara 1
+    if (arryCategory.includes(getBook.category)) {
+       // console.log("Buku sudah di masukan di arryCategory")
+    } else {
+        arryCategory.push(getBook.category)
+    }
+
+    // cara 2
+    if (!arryCategory.includes(getBook.category)) {
+        arryCategory.push(getBook.category)
+    }
 }
